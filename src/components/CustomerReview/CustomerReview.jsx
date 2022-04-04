03565d7a37e0b94d004c8eceb46d6f2dd9782632
ZIHAD22 <mdzihad2244@gmail.com>
2022-04-04 22:44:12 +0600

    add some change to customer review router

--- src/components/CustomerReview/CustomerReview.jsx
+++ src/components/CustomerReview/CustomerReview.jsx
@@ -5,12 +5,12 @@ import ReviewItem from "../ReviewItem/ReviewItem";
 const CustomerReview = ({ short }) => {
   const [reviews, setReviews] = useReview();
   return (
-    <div className="mb-9">
+    <div className="mb-9 min-h-[705px] flex flex-col justify-center">
       <h1 className="text-center my-7 text-5xl font-serif">
-        Customer Say About Products{" "}
+        Customer Say About Products
         <span className="text-orange-500">({reviews.length})</span>
       </h1>
-      <div className="grid grid-cols-3 gap-7 p-6 min-h-[705px]">
+      <div className="grid grid-cols-3 gap-7 p-6">
         {reviews.map((review) => (
           <ReviewItem key={review.id} review={review} />
         ))}
